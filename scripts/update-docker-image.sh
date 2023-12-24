7ba530addeb2f1922916d549c3c53bf7068e36dc
Tyler Lee <tlee75@asu.edu>
2023-12-23 19:08:01 -0700

    Parameterized deploy node

--- scripts/update-docker-image.sh
+++ scripts/update-docker-image.sh
@@ -3,4 +3,4 @@
 
 echo "Updating docker image to: ${IMAGE_TAG}"
 
-ssh ${DOCKER_NODE} "sudo docker stop interview && sudo docker rm interview && sudo docker run -it --name interview tlee75/interview:${IMAGE_TAG}"
+ssh ${DEPLOY_NODE} "sudo docker stop interview && sudo docker rm interview && sudo docker run -it --name interview tlee75/interview:${IMAGE_TAG}"
