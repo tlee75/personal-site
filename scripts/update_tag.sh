531548552ebafa75e63c9d96c4ad5862ef3f3710
Tyler Lee <103002386+tlee75@users.noreply.github.com>
2023-12-24 21:00:35 -0700

    Run docker image listening to port

--- scripts/update_tag.sh
+++ scripts/update_tag.sh
@@ -7,4 +7,4 @@ sudo docker ps -a
 sudo docker image ls
 sudo docker stop interview
 sudo docker rm interview
-sudo docker run --name interview tlee75/interview:${IMAGE_TAG}
+sudo docker run --name interview tlee75/interview:${IMAGE_TAG} -p 80:80
