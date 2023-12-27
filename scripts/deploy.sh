c3f22fd2d30262aed8452d21561929b658ffee6f
Tyler Lee <103002386+tlee75@users.noreply.github.com>
2023-12-26 17:24:16 -0700

    Added basic error handling for the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,13 @@
 echo "Updating docker image to: ${IMAGE_TAG}"
 
 # node1 must match the SSH host in the Github Environment
-ssh -q -o ConnectTimeout=10 -T node1 "IMAGE_TAG=${IMAGE_TAG} DOCKERHUB_ACCOUNT=${DOCKERHUB_ACCOUNT} DOCKERHUB_REPO=${DOCKERHUB_REPO} bash -s" < ./scripts/update_tag.sh
+ssh -o ConnectTimeout=10 -T node1 "IMAGE_TAG=${IMAGE_TAG} DOCKERHUB_ACCOUNT=${DOCKERHUB_ACCOUNT} DOCKERHUB_REPO=${DOCKERHUB_REPO} bash -s" < ./scripts/update_tag.sh
 
-echo "Done"
+DEPLOY_STATUS=$?
+
+if [ "${DEPLOY_STATUS}" -ne 0]
+then
+  echo "ERROR: ${DEPLOY_STATUS}"
+else
+  echo "Done"
+fi
