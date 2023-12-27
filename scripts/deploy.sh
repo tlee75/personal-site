39cec179df47f1e9bc13f57c52b8de09a5ded44d
Tyler Lee <tlee75@asu.edu>
2023-12-26 17:19:40 -0700

    Parameterized docker account and repo scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@
 
 echo "Updating docker image to: ${IMAGE_TAG}"
 
-ssh -q -o ConnectTimeout=10 -T ${DEPLOY_NODE} "IMAGE_TAG=${IMAGE_TAG} bash -s" < ./scripts/update_tag.sh
+# node1 must match the SSH host in the Github Environment
+ssh -q -o ConnectTimeout=10 -T node1 "IMAGE_TAG=${IMAGE_TAG} DOCKERHUB_ACCOUNT=${DOCKERHUB_ACCOUNT} DOCKERHUB_REPO=${DOCKERHUB_REPO} bash -s" < ./scripts/update_tag.sh
 
 echo "Done"
