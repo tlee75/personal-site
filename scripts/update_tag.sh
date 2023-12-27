cbc7e42102b6fcb9cb8a98bc5a8817e366505ee3
Tyler Lee <103002386+tlee75@users.noreply.github.com>
2023-12-26 21:01:35 -0700

    Cleaned up some old scripting

--- scripts/update_tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set +e
-set -x
-
-sudo docker stop ${MKDOCS_APP_NAME}
-sudo docker rm ${MKDOCS_APP_NAME}
-sudo docker run -d -p 8000:8000 --name ${MKDOCS_APP_NAME} ${DOCKERHUB_ACCOUNT}/${MKDOCS_APP_NAME}:${IMAGE_TAG}
-sudo docker ps
